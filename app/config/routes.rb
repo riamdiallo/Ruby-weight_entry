2f657349ff9445253524c7dddfb2429477072c18
riamdiallo <kouratongo55@gmail.com>
2025-02-22 14:09:18 +0100

    premier commit

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root "weight_entries#index"
-  resources :weight_entries
-end
+    devise_for :users,
+    controllers: {
+      omniauth_callbacks: 'omniauth_callbacks'
+     }
+  
+    get "weight_entries/index"
+     root "weight_entries#index"
+     resources :weight_entries
+  end
